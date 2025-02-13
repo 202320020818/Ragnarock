92b37e4091f42edf3a10dec18deec90a59422c93
202320020818 <138109759+202320020818@users.noreply.github.com>
2025-02-13 14:27:35 +0530

    Initial Commit

---
+++ 1.js
@@ -0,0 +1,5 @@
+const Wrapper = document.querySelector('.Wrapper');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', () => Wrapper.classList.add('active'));
+
